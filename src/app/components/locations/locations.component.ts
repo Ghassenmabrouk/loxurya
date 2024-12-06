e433f38b9a28b1987d0099fee9285d0ed56e4aaa
Ghasssen Mabrouk <132289357+Ghassenmabrouk@users.noreply.github.com>
2024-12-06 21:46:32 +0100

    Update locations.component.ts

--- src/app/components/locations/locations.component.ts
+++ src/app/components/locations/locations.component.ts
@@ -15,6 +15,74 @@ export class LocationsComponent {
       phone: '+216 XX XXX XXX'
     },
     {
+      name: 'Salzburg ',
+      image: 'assets/images/locations/tunisia.jpg',
+      description: 'Experience luxury in the heart of Germany',
+      address: 'menchen, Germany',
+      phone: '+216 XX XXX XXX'
+    },
+      {
+      name: 'München',
+      image: 'assets/images/locations/tunisia.jpg',
+      description: 'Experience luxury in the heart of Germany',
+      address: 'menchen, Germany',
+      phone: '+216 XX XXX XXX'
+    },
+
+  {
+      name: 'Innsbruck ',
+      image: 'assets/images/locations/tunisia.jpg',
+      description: 'Experience luxury in the heart of Germany',
+      address: 'menchen, Germany',
+      phone: '+216 XX XXX XXX'
+    },
+
+
+      {
+      name: 'Bregenz',
+      image: 'assets/images/locations/tunisia.jpg',
+      description: 'Experience luxury in the heart of Germany',
+      address: 'menchen, Germany',
+      phone: '+216 XX XXX XXX'
+    },
+
+
+      {
+      name: 'Budapest ',
+      image: 'assets/images/locations/tunisia.jpg',
+      description: 'Experience luxury in the heart of Germany',
+      address: 'menchen, Germany',
+      phone: '+216 XX XXX XXX'
+    },
+     {
+      name: 'Brno  ',
+      image: 'assets/images/locations/tunisia.jpg',
+      description: 'Experience luxury in the heart of Germany',
+      address: 'menchen, Germany',
+      phone: '+216 XX XXX XXX'
+    },
+     {
+      name: 'Budapest ',
+      image: 'assets/images/locations/tunisia.jpg',
+      description: 'Experience luxury in the heart of Germany',
+      address: 'menchen, Germany',
+      phone: '+216 XX XXX XXX'
+    },
+     {
+      name: 'Lubijana ',
+      image: 'assets/images/locations/tunisia.jpg',
+      description: 'Experience luxury in the heart of Germany',
+      address: 'menchen, Germany',
+      phone: '+216 XX XXX XXX'
+    },
+     {
+      name: 'Prag ',
+      image: 'assets/images/locations/tunisia.jpg',
+      description: 'Experience luxury in the heart of Germany',
+      address: 'menchen, Germany',
+      phone: '+216 XX XXX XXX'
+    },
+    {
       name: 'Vienna',
       image: 'assets/images/locations/vienna.jpg',
       description: 'Premium service in the cultural capital',
